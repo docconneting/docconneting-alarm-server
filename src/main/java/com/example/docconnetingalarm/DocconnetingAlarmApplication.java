708ba0854658870f2c608e0d29de6df7936b5338
kyn1013 <yn112511@naver.com>
2025-04-23 17:03:01 +0900

    Init : 프로젝트 초기 세팅 (#1)

---
+++ src/main/java/com/example/docconnetingalarm/DocconnetingAlarmApplication.java
@@ -0,0 +1,13 @@
+package com.example.docconnetingalarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocconnetingAlarmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocconnetingAlarmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/docconnetingalarm/DocconnetingAlarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.docconnetingalarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocconnetingAlarmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
